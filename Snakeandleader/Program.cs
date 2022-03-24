78e7a208f8c6523eae71ad2d83ce8b41274aeb7b
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-24 13:42:07 +0530

    [Snake and Leader] Added : Welcome massage

---
+++ Snakeandleader/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snakeandleader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Leader Simulator");
+            Console.ReadLine();
+        }
+    }
+}
