ec607aa9065856dbc8cceff057322a7b8b7e3384
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-25 14:01:16 +0530

    [Snake and Leader] Added : after every die role

--- Snakeandleader/Program.cs
+++ Snakeandleader/Program.cs
@@ -18,13 +18,9 @@ namespace Snakeandleader
             Random random = new Random();
             int dice1 = random.Next(1, 7);
             Console.WriteLine("The Random Dice number is :" +dice1);
-<<<<<<< HEAD
-=======
-
             Console.ReadLine();
-            Console.WriteLine("The Random Dice number is : "+dice1);
+
             
->>>>>>> UC-5
         }
         
 
@@ -60,11 +56,9 @@ namespace Snakeandleader
                         break;
                     }
             }
-<<<<<<< HEAD
-=======
+            Console.ReadLine ();
 
 
->>>>>>> UC-5
         }
         public static void UC4()
         {
@@ -112,9 +106,6 @@ namespace Snakeandleader
 
             }
             Console.WriteLine("Player wins" + count);
-<<<<<<< HEAD
-
-=======
             Console.ReadLine();
         }
 
@@ -168,8 +159,69 @@ namespace Snakeandleader
                 }
 
             }
+            
             Console.WriteLine("Player wins" + count);
->>>>>>> UC-5
+            Console.ReadLine();
+        }
+
+        public static void UC6()
+        {
+            int ladder = 1, ldice = 0;
+            int snake = 2, sdice;
+            int win = 100;
+            int diceRoll = 0;
+            int count = 0, a = 0;
+            Console.WriteLine("Welcome to UC_6");
+            while (count != win)
+            {
+                if (count >= 0 && count < 100)
+                {
+                    Random random = new Random();
+                    int randomCheck = random.Next(3);
+                    diceRoll++;
+
+                    switch (randomCheck)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine("Got Ladder");
+                                Random rnd = new Random();
+                                ldice = rnd.Next(1, 7);
+                                count = count + ldice;
+                                Console.WriteLine("Position is " + count);
+                                a = count;
+                                break;
+                            }
+                        case 2:
+                            {
+                                Random sran = new Random();
+                                sdice = sran.Next(1, 7);
+                                count = count - sdice;
+                                Console.WriteLine("Position is: " + count);
+                                Console.WriteLine("Got snake");
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("No play");
+                                Console.WriteLine("Position is: " + count);
+                                break;
+                            }
+                    }
+                }
+                else if (a > 100)
+                {
+                    count = a - ldice;
+                }
+                else
+                {
+                    count = 0;
+                }
+
+            }
+            Console.WriteLine("Player wins" + count);
+            Console.WriteLine("Number of time Dice Roll is to win: " + diceRoll);
+            Console.ReadLine();
         }
 
         static void Main(string[] args)
@@ -179,6 +231,7 @@ namespace Snakeandleader
             Program.UC3();
             Program.UC4();
             Program.UC5();
+            Program.UC6();
         }
     }
 }
