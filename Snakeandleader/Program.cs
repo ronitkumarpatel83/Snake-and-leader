6062a0350df6b9bc1f7d0a7f72a84a6f223bae5c
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-25 12:45:23 +0530

    [Snake and Leader] Added : 100 winning position

--- Snakeandleader/Program.cs
+++ Snakeandleader/Program.cs
@@ -16,9 +16,46 @@ namespace Snakeandleader
             Console.WriteLine("Welcome to UC2");
 
             Random random = new Random();
-            int diceNumber = random.Next(1, 7);
-            Console.WriteLine($"The Random Dice number is : {diceNumber}");
-            Console.ReadLine();
+            int dice1 = random.Next(1, 7);
+            Console.WriteLine("The Random Dice number is : "+dice1);
+            
+
+        }
+        
+
+    
+
+        public static void UC3()
+        {
+            int ladder = 1, ldice;
+            int snake = 2, sdice;
+            Console.WriteLine("Welcome to UC3");
+            Random random = new Random();
+            int randomCheck = random.Next(3);
+
+            switch (randomCheck)
+            {
+                case 1:
+                    {
+                        Random rnd = new Random();
+                        ldice = rnd.Next(1, 7);
+                        Console.WriteLine("Got Ladder");
+                        break;
+                    }
+                case 2:
+                    {
+                        Random sran = new Random();
+                        sdice = sran.Next(1, 7);
+                        Console.WriteLine("Got snake");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("No play, Stay at base");
+                        break;
+                    }
+            }
+
 
         }
 
@@ -26,6 +63,7 @@ namespace Snakeandleader
         {
             Program.UC1();
             Program.UC2();
+            Program.UC3();
         }
     }
 }
