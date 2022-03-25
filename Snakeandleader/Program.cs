b2ccd641598f57e2ca96903cf64950f0129cfc64
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-25 12:21:30 +0530

    [Snake and Leader] Added : Dice rolling

--- Snakeandleader/Program.cs
+++ Snakeandleader/Program.cs
@@ -16,8 +16,8 @@ namespace Snakeandleader
             Console.WriteLine("Welcome to UC2");
 
             Random random = new Random();
-            int diceNumber = random.Next(1, 7);
-            Console.WriteLine($"The Random Dice number is : {diceNumber}");
+            int dice1 = random.Next(1, 7);
+            Console.WriteLine("The Random Dice number is :" +dice1);
             Console.ReadLine();
 
         }
